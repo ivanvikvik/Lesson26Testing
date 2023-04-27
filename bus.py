61bc3cb7e5d991c3db464265d7f8b80ec0458ab1
ivanvikvik <ivanvikvik@gmail.com>
2023-04-27 20:30:00 +0300

    Using private fields with interface get/set methods

--- bus.py
+++ bus.py
@@ -4,19 +4,23 @@
 
 class Bus:
     def __init__(self, brand='no name', price=0, number=0):
-        self.__brand = brand          # private
-        self.__price = price          # private
-        self.__number = number        # private
+        self.__brand = brand  # private
+        self.__price = price  # private
+        self.__number = number  # private
 
+    def get_brand(self):
+        return self.__brand
+    def set_brand(self, name):
+        self.__brand = name
+    def get_number(self):
+        return self.__number
+    def set_number(self, number):
+        self.__number = number
     def get_price(self):
         return self.__price
-
     def set_price(self, price):
         if price > 0:
             self.__price = price
-
-    def __method(self):
-        pass
 
     def __str__(self):
         return f"Bus: {self.__brand}, price = ${self.__price}," \
@@ -25,5 +29,8 @@ class Bus:
 
 if __name__ == "__main__":
     bus = Bus()
-    bus.__method()
-    print(bus.__number)
+    # bus.price = 12000
+    # print(bus.price)
+
+    bus.set_price(12000)
+    print(bus.get_price())
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from bus import *
 from manager import Manager
 
+x = 10
 
 def main():
     bus1 = Bus("Scania", 23000, 42)
@@ -8,6 +9,8 @@ def main():
     bus3 = Bus("Volvo", 31000, 48)
     bus4 = Bus("PAZ", 11000, 15)
     bus5 = Bus("Mercedes Benz", 123000, 67)
+
+    y = x
 
     buses = (bus1, 5, bus2, 6.7, bus3, bus4, "dkjnfsdkj", bus5)
 
